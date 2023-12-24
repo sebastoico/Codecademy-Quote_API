effa927648fa08b12947cf5ad0623d1cbabd90db
Sebastián Jaramillo <108204749+sebastoico@users.noreply.github.com>
2023-12-24 15:59:59 +0000

    Project Requirements #5 Completed

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ app.use(express.static('public'));
 
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = getRandomElement(quotes);
-    res.send(randomQuote);
+    res.send({ quote: randomQuote });
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    const person = req.query.person;
+    if (person) {
+        const quotesByPerson = quotes.filter(quote => quote.person === person);
+        res.send({ quotes: quotesByPerson });
+    } else {
+        res.send({ quotes: quotes });
+    }
 })
 
 app.listen(PORT, () => console.log('Server is running'));
