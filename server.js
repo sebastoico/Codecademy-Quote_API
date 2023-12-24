d9375d0a58ee08ad63cebd3f25b2779e4f5f48e1
Sebastián Jaramillo <108204749+sebastoico@users.noreply.github.com>
2023-12-24 15:40:25 +0000

    Project Requirements #4 Completed

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send(randomQuote);
+})
+
 app.listen(PORT, () => console.log('Server is running'));
